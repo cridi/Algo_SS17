8513e754d24be8179b16a30991bf6af6ea5bf689
Knoedelchan <denni.dette@web.de>
2017-05-27 18:21:16 +0200

    String processing without error checks

--- user.cpp
+++ user.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 
 #ifndef _USE_OLD_OSTREAMS
 using namespace std;
@@ -150,39 +151,186 @@ void Restart()
 	clrscr();
 	printf("######################################\n\n");
 }
+
+//USER PROGRAMM
+string BauteilToken, NetworkToken;
+
+class Network {
+	string INPUT;
+	string OUTPUT;
+	string CMN;
+	vector<string> INTERNALS;
+public:
+	Network();
+	Network(string INPUT, string OUTPUT, string CMN, vector<string> INTERNALS);
+};
+Network::Network() {}
+Network::Network(string a, string b, string c, vector<string> d) {
+	INPUT = a;
+	OUTPUT = b;
+	CMN = c;
+	INTERNALS = d;
+}
+
+class Bauteil {
+	string Name;
+	string Art;
+	string Pin1;
+	string Pin2;
+public:
+	Bauteil(string Name, string Art, string Pin1, string Pin2);
+};
+Bauteil::Bauteil(string a, string b, string c, string d){
+	Name = a;
+	Art = b;
+	Pin1 = c;
+	Pin2 = d;
+}
+Network Netzwerk;
+vector<Bauteil*> Bauteile;
  
 //USER FUNCTIONS
-int TokenSum(string NetworkBuffer) {
+string NetworkInput() {
+	string InputBuffer;
 	string SpaceLess = "";
-	string seperator = ";";
-	string token;
+	string separator = ";\n";
+	string Token;
 
 	int endpos = 0;
 	int startpos, spacepos = 0;
 	int wordCount = 1;
 
-	cout << "Enter Network in Format: a:IN; b:Out; c: CMN; d,e: Internal;\n" << "Nets:\t";
-	getline(cin, NetworkBuffer);
-	cout << "Registered String:\t" << NetworkBuffer << endl;
+	cout << "Enter String in Format: a:IN; b:Out; c: CMN; d,e: Internal;\n" << "Nets:\t";
+	getline(cin, InputBuffer);
+	cout << "Registered String:\t" << InputBuffer << endl;
 
-
-	while (1){
-		for (spacepos; spacepos < NetworkBuffer.size(); spacepos++) {
-			if (isspace(NetworkBuffer[spacepos])) spacepos++;
-			cout << NetworkBuffer[spacepos];
-			SpaceLess = SpaceLess + NetworkBuffer[spacepos];
+	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
+		if (isspace(InputBuffer[spacepos])) {
+			spacepos++;
+			if (spacepos == InputBuffer.size()) break;
 		}
+		SpaceLess = SpaceLess + InputBuffer[spacepos];
+	}
+	cout << SpaceLess << "\n";
 
-		startpos = SpaceLess.find_first_not_of(seperator, endpos);
-		if (startpos == SpaceLess.npos)return -1;
-		endpos = SpaceLess.find_first_of(seperator, startpos);
-		if (endpos == SpaceLess.npos)return -1;
-		token = SpaceLess.substr(startpos, endpos - startpos);
+	//while (1){
+	//	startpos = SpaceLess.find_first_not_of(separator, endpos);
+	//	if (startpos == SpaceLess.npos)break;
+	//	endpos = SpaceLess.find_first_of(separator, startpos);
+	//	if (endpos == SpaceLess.npos)break;
+	//	token = SpaceLess.substr(startpos, endpos - startpos);
 
-		cout << endl << token;
+	//	cout  << token << endl;
+	//}
+
+	return SpaceLess;
+}
+string BauteilInput() {
+	string InputBuffer;
+	string SpaceLess = "";
+	string separator = ";\n";
+	string Token;
+
+	int endpos = 0;
+	int startpos, spacepos = 0;
+	int wordCount = 1;
+
+	cout << "Enter String in Format: R1:R(a, d); C2:C(d, b); L4:L(b, c)";
+	getline(cin, InputBuffer);
+	cout << "Registered String:\t" << InputBuffer << endl;
+
+	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
+		if (isspace(InputBuffer[spacepos])) {
+			spacepos++;
+			if (spacepos == InputBuffer.size()) break;
+		}
+		SpaceLess = SpaceLess + InputBuffer[spacepos];
+	}
+	cout << SpaceLess << "\n";
+
+	//while (1){
+	//	startpos = SpaceLess.find_first_not_of(separator, endpos);
+	//	if (startpos == SpaceLess.npos)break;
+	//	endpos = SpaceLess.find_first_of(separator, startpos);
+	//	if (endpos == SpaceLess.npos)break;
+	//	token = SpaceLess.substr(startpos, endpos - startpos);
+
+	//	cout  << token << endl;
+	//}
+
+	return SpaceLess;
+}
+int TokenNetwork(string Token) {
+	int startPos = 0, endPos = 0;
+	int CommaPosCheck;
+	string separator = ";";
+	string subToken[3];
+	vector<string> Internals;
+
+	if(Token.find("IN")==Token.npos)return -1;
+	startPos = Token.find_first_not_of(separator, startPos);
+	endPos = Token.find_first_of(':', startPos);
+	subToken[0] = Token.substr(startPos, endPos - startPos);
+
+	if (Token.find("Out") == Token.npos)return -1;
+	startPos = Token.find_first_of(';', endPos);
+	startPos = Token.find_first_not_of(separator, startPos);
+	endPos = Token.find_first_of(':', startPos);
+	subToken[1] = Token.substr(startPos, endPos - startPos);
+
+	if (Token.find("CMN") == Token.npos)return -1;
+	startPos = Token.find_first_of(';', endPos);
+	startPos = Token.find_first_not_of(separator, startPos);
+	endPos = Token.find_first_of(':', startPos);
+	subToken[2] = Token.substr(startPos, endPos - startPos);
+
+	if (Token.find("Internal") == Token.npos)return -1;
+	startPos = Token.find_first_of(';', endPos);
+	startPos = Token.find_first_not_of(separator, startPos);
+	endPos = Token.find_first_of(':', startPos);
+	while (startPos != endPos) {
+
+		CommaPosCheck = Token.find_first_of(',', startPos);
+		if (CommaPosCheck == Token.npos) CommaPosCheck = Token.find_first_of(':', startPos);
+		Internals.push_back(Token.substr(startPos, CommaPosCheck - startPos));
+		startPos = Token.find_first_not_of(',', CommaPosCheck);
 	}
 
+	Netzwerk = Network(subToken[0], subToken[1], subToken[2], Internals);
+	return 0;
 }
+void TokenBauteil(string Token) {
+	int startPos = 0, endPos = 0;
+	char separator;
+	string subToken[4];
+
+	while(startPos != Token.npos) {
+
+		separator = ':';
+		endPos = Token.find_first_of(separator, startPos);
+		subToken[0] = Token.substr(startPos, endPos - startPos);
+		startPos = Token.find_first_not_of(separator, endPos);
+
+		separator = '(';
+		endPos = Token.find_first_of(separator, startPos);
+		subToken[1] = Token.substr(startPos, endPos - startPos);
+		startPos = Token.find_first_not_of(separator, endPos);
+
+		separator = ',';
+		endPos = Token.find_first_of(separator, startPos);
+		subToken[2] = Token.substr(startPos, endPos - startPos);
+		startPos = Token.find_first_not_of(separator, endPos);
+
+		separator = ')';
+		endPos = Token.find_first_of(separator, startPos);
+		subToken[3] = Token.substr(startPos, endPos - startPos);
+		startPos = Token.find_first_of(';', endPos);
+		startPos = Token.find_first_not_of(';', startPos);
+
+		Bauteile.push_back(new Bauteil(subToken[0], subToken[1], subToken[2], subToken[3]));
+	}
+}
+
 
 void user_main()
 {
@@ -192,20 +340,20 @@ void user_main()
 	SetConsoleWindowTop();
 	Sleep(1000);
 
-	//USER PROGRAMM
-	string NetworkBuffer;
-	string CircuitBuffer;
-
-
 	while (1) {								// Endlosschleife
 		get_windowsize(&ww, &hh);
 		set_drawarea(ww, hh);				// Setzen des Zeichenbereiches     
 		clrscr();
 
-		TokenSum(NetworkBuffer);			//Getting rid of spaces and cutting in blocks separated by ;
+		NetworkToken = NetworkInput();			//Getting rid of spaces and cutting in blocks separated by ;
+		//BauteilToken = BauteilInput();
+
+	//R1:R(a, b);
+		TokenNetwork(NetworkToken);
+		//TokenBauteil(BauteilToken);
 
 		Restart();							// Den "Restart"-Button malen und auf eine Aktivierung warten.
 		if(StopProcess())break;
-		
+
 	}
 }
