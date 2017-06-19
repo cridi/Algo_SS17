4296ae303c409e343697c58206a975c3bcda0ff0
Knoedelchan <denni.dette@web.de>
2017-06-19 14:19:23 +0200

    back 2 da r00tz

--- user.cpp
+++ user.cpp
@@ -25,8 +25,8 @@ using namespace std;
 #define _sinus(winkel)		sin((winkel) / _180_durch_pi)	// Funktion im Gradmass.
 #define _cosinus(winkel)	cos((winkel) / _180_durch_pi)	// Funktion im Gradmass.
 
-COLORREF Colref[]={BLACK,RED,GREEN,BLUE,YELLOW,BROWN};
-int Colind=0;
+COLORREF Colref[] = { BLACK,RED,GREEN,BLUE,YELLOW,BROWN };
+int Colind = 0;
 
 //-------------------------------------------------------------------------------------------------------//
 const int IDENTIFIER = 4;
@@ -60,107 +60,16 @@ void CParser::init_TokenTable() {
 	//newTokenEntry("L", ii++);
 }
 
-
-struct _BAUM {							// Die fuenf Eingangsparameter des Baumes.
-	int		Tiefe;
-	int		Neigung_links;
-	float	Wachstum_links;
-	int		Neigung_rechts;
-	float	Wachstum_rechts;
-	void WerteKorrigieren()
-	{
-		if (Wachstum_links > 1) Wachstum_links = 1 / Wachstum_links;
-		if (Wachstum_rechts > 1) Wachstum_rechts = 1 / Wachstum_rechts;
-		Neigung_rechts = -Neigung_rechts;
-	};
-} baum;
-
-
-void Zeichne_Ast(int x, int y, float n, float Tiefe, float Laenge)
-{
-	int x_rel;
-	int y_rel;
-	if(StopProcess())return;
-	if (Tiefe > 1) {										// Stopbedingung fuer die Rekursion.
-		// Zeichnen des linken Astes.
-		x_rel = (int)(Laenge * _sinus(n + baum.Neigung_links));	// Berechnen der x-Koordinate.
-		y_rel = (int)(Laenge * _cosinus(n + baum.Neigung_links));	// Berechnen der y-Koordinate.
-		COLORREF cref=Colref[Colind++]; if(Colind>5)Colind=0;
-		lineto(x-x_rel, y-y_rel, cref);					// Zeichnen
-		// Rekursion!! Zeichnen des nachfolgenden Astes.
-		//Sleep(5);updatescr();
-		Zeichne_Ast(x-x_rel, y-y_rel, n+baum.Neigung_links, Tiefe-1, Laenge*baum.Wachstum_links);
-		moveto(x,y);										// Cursor zur Anfangsposition zuruecksetzen.
-
-		// Zeichnen des rechten Astes.
-		x_rel = (int)(Laenge * _sinus(n + baum.Neigung_rechts));
-		y_rel = (int)(Laenge * _cosinus(n + baum.Neigung_rechts));
-		cref=Colref[Colind++];if(Colind>5)Colind=0;
-		lineto(x-x_rel, y-y_rel, cref);
-		//Sleep(5);updatescr();
-		Zeichne_Ast(x-x_rel, y-y_rel, n+baum.Neigung_rechts, Tiefe-1, Laenge*baum.Wachstum_rechts);
-	}
-
-}
-
-void Zeichne_Baum()
-{
-	int Weite, Hoehe, x, y, StammLaenge;
-
-	// Berechnen des Gesamtwachstums.
-	float GesamtWachstum = 1;
-	float Wachstum = (baum.Wachstum_links > baum.Wachstum_rechts) ? baum.Wachstum_links : baum.Wachstum_rechts;
-	float WT = 1;
-	for (int i=1; i<baum.Tiefe; i++) {
-		WT *= Wachstum;
-		GesamtWachstum += WT;
-	}
-
-	// Berechnung des Fusspunktes des Baumes
-	get_windowsize(&Weite, &Hoehe);
-	x = Weite / 2;
-	y = Hoehe*3/4;
-	text(x, y, 18, RED, "Wurzel [x,y]=[%d,%d]", x,y);
-
-	StammLaenge = (int)(y / GesamtWachstum);			// Laenge des Stammes.
-
-	moveto(x, y);									// Marker auf die Wurzel setzen.
-	lineto(x, y - StammLaenge, BLACK);				// Stamm zeichnen
-	Zeichne_Ast(x, y - StammLaenge, 0, (float)baum.Tiefe, (float)(StammLaenge*(baum.Wachstum_links + baum.Wachstum_rechts)/2));
-}
-
-void Restart()
-{
-	int b, h, x, y;
-
-	get_drawarea(&b, &h);
-
-	textbox(b - 120, h - 40, b - 5, h - 5, 18, BLUE, GREY, GREY, SINGLE_LINE | VCENTER_ALIGN | CENTER_ALIGN, ("Restart"));
-	updatescr();
-
-	while ( 
-		!((mouseclick(&x,&y) == 1) &&
-		  ((x > b-120) && (x < b-5)) &&
-		  ((y > h-40)  &&	(y < h-5))
-		 )) {
-		printf(".");
-		if(StopProcess())break;
-	};
-
-	printf("######################################\n\n");
-	clrscr();
-	printf("######################################\n\n");
-}
-
 //USER PROGRAMM
 string BauteilToken, NetworkToken;
 
 class Network {
+public:
 	string INPUT;
 	string OUTPUT;
 	string CMN;
 	vector<string> INTERNALS;
-public:
+
 	Network();
 	Network(string INPUT, string OUTPUT, string CMN, vector<string> INTERNALS);
 };
@@ -173,22 +82,216 @@ Network::Network(string a, string b, string c, vector<string> d) {
 }
 
 class Bauteil {
+public:
 	string Name;
 	string Art;
 	string Pin1;
 	string Pin2;
-public:
+
 	Bauteil(string Name, string Art, string Pin1, string Pin2);
+	string getName();
+	string getArt();
+	string getPin1();
+	string getPin2();
 };
-Bauteil::Bauteil(string a, string b, string c, string d){
+
+Bauteil::Bauteil(string a, string b, string c, string d) {
 	Name = a;
 	Art = b;
 	Pin1 = c;
 	Pin2 = d;
 }
+string Bauteil::getName() {
+	return Name;
+}
+string Bauteil::getArt() {
+	return Art;
+}
+string Bauteil::getPin1() {
+	return Pin1;
+}
+string Bauteil::getPin2() {
+	return Pin1;
+}
 Network Netzwerk;
 vector<Bauteil*> Bauteile;
- 
+vector<Bauteil*> serial_Bauteile;
+
+void Is_parallel() {
+	int outerSweep = Bauteile.size();
+	int innerSweep = Bauteile.size();
+
+	for (int i = 0; i < outerSweep; i++) {
+		for (int ii = 0; ii < innerSweep; ii++) {
+
+			if (i == ii) {
+				ii++;
+				if (ii == innerSweep)break;
+			}
+
+			if (((Bauteile.at(i)->Pin2 == Bauteile.at(ii)->Pin1) && (Bauteile.at(i)->Pin1 == Bauteile.at(ii)->Pin2)) ||
+				((Bauteile.at(i)->Pin2 == Bauteile.at(ii)->Pin2) && (Bauteile.at(ii)->Pin1 == Bauteile.at(i)->Pin1))) {			//Detection is parallel?
+
+				if (Bauteile.at(i)->Art == Bauteile.at(ii)->Art) {
+					if (Bauteile.at(i)->Art == "C") {
+						Bauteile.push_back(new Bauteil("(" + Bauteile.at(i)->Name + "+" + Bauteile.at(ii)->Name + ")",
+							Bauteile.at(i)->Art, Bauteile.at(i)->Pin1, Bauteile.at(i)->Pin2));
+
+					}
+					else {
+						Bauteile.push_back(new Bauteil(Bauteile.at(i)->Name + "||" + Bauteile.at(ii)->Name,
+							Bauteile.at(i)->Art, Bauteile.at(i)->Pin1, Bauteile.at(i)->Pin2));				//New Element with outer Pins and new Name
+					}
+
+					if (i < ii) {
+						Bauteile.erase(Bauteile.begin() + i);													//Deleting obsolete Elements
+						Bauteile.erase(Bauteile.begin() + ii - 1);
+					}
+					else {
+						Bauteile.erase(Bauteile.begin() + ii);													//Deleting obsolete Elements
+						Bauteile.erase(Bauteile.begin() + i - 1);
+					}
+
+					//		leftSweep = floor(Bauteile.size() / 2);
+					//	rightSweep = ceil(Bauteile.size() / 2);
+					//	ii = Bauteile.size() - 1;
+					return;
+				}
+			}
+		}
+	}
+
+}
+
+
+bool Is_serial_Pin(string Pin_to_check) {
+	int checksum = 0;
+
+	for (int i = 0; i < Netzwerk.INTERNALS.size(); i++) {
+
+		if (!Pin_to_check.compare(Netzwerk.INTERNALS.at(i))) {		//Check if IN OUT or CMN
+
+			for (unsigned int i = 0; i < Bauteile.size(); i++) {
+				if (!((Bauteile.at(i)->Pin1).compare(Pin_to_check))) { checksum++; };												//Check if only two times connected
+				if (!((Bauteile.at(i)->Pin2).compare(Pin_to_check))) { checksum++; };												//Then it ist realy serial
+			}
+		}
+	}
+
+	if (checksum != 2) return false;
+	if (checksum == 2) return true;
+}
+
+void find_serial_Bauteil(string serial_Pin) {
+	for (unsigned int i = 0; i < Bauteile.size(); i++) {
+		if ((!((Bauteile.at(i)->Pin1).compare(serial_Pin))) || (!((Bauteile.at(i)->Pin2).compare(serial_Pin)))) {
+			serial_Bauteile.push_back(Bauteile.at(i));
+			break;
+		}
+	}
+}
+
+void reverse_find_serial_Bauteil(string serial_Pin) {
+	for (unsigned int i = Bauteile.size() - 1; i >= 0; i--) {
+		if ((!((Bauteile.at(i)->Pin1).compare(serial_Pin))) || (!((Bauteile.at(i)->Pin2).compare(serial_Pin)))) {
+			serial_Bauteile.push_back(Bauteile.at(i));
+			break;
+		}
+	}
+}
+
+void find_outer_Pins(string serial_Pin) {
+	if ((serial_Pin.compare(serial_Bauteile.at(0)->Pin2))) {
+		(serial_Bauteile.at(0)->Pin1) = (serial_Bauteile.at(0)->Pin2);
+	}
+	if ((serial_Pin.compare(serial_Bauteile.at(1)->Pin2))) {
+		(serial_Bauteile.at(1)->Pin1) = (serial_Bauteile.at(1)->Pin2);
+	}
+}
+
+void create_new_serial_Bauteil() {
+
+	if (serial_Bauteile.at(0)->Art == serial_Bauteile.at(1)->Art) {
+
+		if (serial_Bauteile.at(0)->Art == "C") {
+			Bauteile.push_back(new Bauteil(serial_Bauteile.at(0)->Name + "||" + serial_Bauteile.at(1)->Name,
+				serial_Bauteile.at(1)->Art, serial_Bauteile.at(0)->Pin1, serial_Bauteile.at(1)->Pin1));
+		}
+		else {
+			Bauteile.push_back(new Bauteil("(" + serial_Bauteile.at(0)->Name + " + " + serial_Bauteile.at(1)->Name + ")",
+				serial_Bauteile.at(1)->Art, serial_Bauteile.at(0)->Pin1, serial_Bauteile.at(1)->Pin1));				//New Element with outer Pins and new Name
+		}
+
+		for (int i = 0; i < Bauteile.size(); i++) {
+			if (!(serial_Bauteile.at(0)->Name.compare(Bauteile.at(i)->Name)) || (!(serial_Bauteile.at(1)->Name.compare(Bauteile.at(i)->Name)))) {
+				Bauteile.erase(Bauteile.begin() + i);
+				i = -1;
+			}
+		}
+
+		serial_Bauteile.erase(serial_Bauteile.begin() + 1);
+		serial_Bauteile.erase(serial_Bauteile.begin() + 0);
+	}
+}
+
+
+void Is_serial() {
+
+	vector<string*> serial_Pins;
+	int c = 0;
+	int PinFlag = 0;
+
+	for (int i = 0; i < Bauteile.size(); i++) {															//Zuordnung äußere Pins, gemeinsamer Pin;
+		PinFlag = 0;
+		if (Is_serial_Pin(Bauteile.at(i)->Pin1)) {
+
+			for (int ii = 0; ii < serial_Pins.size(); ii++) {
+				if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin1))) PinFlag = 1;
+			}
+
+			if (PinFlag == 0) {
+				serial_Pins.push_back(new string(Bauteile.at(i)->Pin1));
+			}
+		}
+		else if (Is_serial_Pin(Bauteile.at(i)->Pin2)) {
+
+			for (int ii = 0; ii < serial_Pins.size(); ii++) {
+				if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin2))) PinFlag = 1;
+			}
+			if (PinFlag == 0) {
+				serial_Pins.push_back(new string(Bauteile.at(i)->Pin2));
+			}
+		}
+	}
+	for (int i = 0; i < serial_Pins.size(); i++) {
+		find_serial_Bauteil(*serial_Pins.at(i));
+		reverse_find_serial_Bauteil(*serial_Pins.at(i));
+		find_outer_Pins(*serial_Pins.at(i));
+		create_new_serial_Bauteil();
+	}
+
+}
+
+
+void THE_ALGORITHM() {
+	int serialLimit = Bauteile.size();
+	int parallelLimit = Bauteile.size();
+
+	for (int i = 0; i < 5; i++) {
+
+		for (unsigned int i = 0; i < serialLimit; i++) {
+			Is_serial();
+			serialLimit = Bauteile.size();
+		}
+
+		for (unsigned int i = 0; i < parallelLimit; i++) {
+			Is_parallel();
+			parallelLimit = Bauteile.size();
+		}
+	}
+}
+
+
 //USER FUNCTIONS
 string NetworkInput() {
 	string InputBuffer;
@@ -200,10 +303,11 @@ string NetworkInput() {
 	int startpos, spacepos = 0;
 	int wordCount = 1;
 
-	//cout << "Enter String in Format: a:IN; b:Out; c: CMN; d,e: Internal;\n" << "Nets:\t";
+	//cout << "Enter String in Format: a:IN; b:Out; c: CMN; d,e: Internal;\n" << "Nets:\t";		//Disabled for Testing
 	//getline(cin, InputBuffer);
 	//cout << "Registered String:\t" << InputBuffer << endl;
-	InputBuffer = "a:IN; b:Out; c: CMN; d,e: Internal;";
+
+	InputBuffer = "a:IN; b:Out; c: CMN; d,e,f,g: Internal;";			// For Testing only
 
 	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
 		if (isspace(InputBuffer[spacepos])) {
@@ -212,7 +316,7 @@ string NetworkInput() {
 		}
 		SpaceLess = SpaceLess + InputBuffer[spacepos];
 	}
-	cout << SpaceLess << "\n";
+	std::cout << SpaceLess << "\n";
 
 	//while (1){
 	//	startpos = SpaceLess.find_first_not_of(separator, endpos);
@@ -236,11 +340,11 @@ string BauteilInput() {
 	int startpos, spacepos = 0;
 	int wordCount = 1;
 
-	//cout << "Enter String in Format: R1:R(a, d); C2:C(d, b); L4:L(b, c)";
+	//cout << "Enter String in Format: R1:R(a, d); C2:C(d, b); L4:L(b, c)";			// Disabled for Testing
 	//getline(cin, InputBuffer);
 	//cout << "Registered String:\t" << InputBuffer << endl;
-	InputBuffer = "R1:R(a, d); C2:C(d, b); L4:L(b, c);";
 
+	InputBuffer = "R1:R(a, d); L2:L(d, f); L3:L(d, f); R4:R(d, f); R5:R(d, e); R6:R(e, f); R7:R(f, b);C1:C(d,f); C2:C(d,g); C3:C(f,g);";			// For Testing only 
 	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
 		if (isspace(InputBuffer[spacepos])) {
 			spacepos++;
@@ -269,7 +373,7 @@ int TokenNetwork(string Token) {
 	string subToken[3];
 	vector<string> Internals;
 
-	if(Token.find("IN")==Token.npos)return -1;
+	if (Token.find("IN") == Token.npos)return -1;
 	startPos = Token.find_first_not_of(separator, startPos);
 	endPos = Token.find_first_of(':', startPos);
 	subToken[0] = Token.substr(startPos, endPos - startPos);
@@ -306,7 +410,7 @@ void TokenBauteil(string Token) {
 	char separator;
 	string subToken[4];
 
-	while(startPos != Token.npos) {
+	while (startPos != Token.npos) {
 
 		separator = ':';
 		endPos = Token.find_first_of(separator, startPos);
@@ -333,29 +437,42 @@ void TokenBauteil(string Token) {
 	}
 }
 
+void print_network() {
+	for (unsigned int i = 0; i < Bauteile.size(); i++) {
+		std::cout << Bauteile.at(i)->Name << ":" << Bauteile.at(i)->Art << "(" << Bauteile.at(i)->Pin1 << ":" << Bauteile.at(i)->Pin2 << ")" << "\t";
+	}
+}
+
 
 void user_main()
 {
-	int ww,hh;
+	int ww, hh;
 	set_windowpos(0, 0, 600, 400);
 
 	SetConsoleWindowTop();
 	Sleep(1000);
 
-	while (1) {								// Endlosschleife
-		get_windowsize(&ww, &hh);
-		set_drawarea(ww, hh);				// Setzen des Zeichenbereiches     
-		clrscr();
+	//while (1) {								// Endlosschleife
+	get_windowsize(&ww, &hh);
+	set_drawarea(ww, hh);				// Setzen des Zeichenbereiches     
+	clrscr();
 
-		NetworkToken = NetworkInput();			//Getting rid of spaces and cutting in blocks separated by ;
-		//BauteilToken = BauteilInput();
+	NetworkToken = NetworkInput();			//Getting rid of spaces and cutting in blocks separated by ;
+	BauteilToken = BauteilInput();
 
 	//R1:R(a, b);
-		TokenNetwork(NetworkToken);
-		//TokenBauteil(BauteilToken);
+	TokenNetwork(NetworkToken);
+	TokenBauteil(BauteilToken);
 
-		Restart();							// Den "Restart"-Button malen und auf eine Aktivierung warten.
-		if(StopProcess())break;
+	THE_ALGORITHM();
 
-	}
+	print_network();
+
+
+
+
+	// Den "Restart"-Button malen und auf eine Aktivierung warten.
+	//if(StopProcess())break;
+
+	//}
 }
