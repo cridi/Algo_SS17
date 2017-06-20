6af95d7e9d5f46e5106b53f035c71c5a89e142d0
Knoedelchan <denni.dette@web.de>
2017-06-20 14:39:06 +0200

    Super Geiler Shit / Ohne ÜFKT und Fehlerabfrage

--- user.cpp
+++ user.cpp
@@ -1,4 +1,4 @@
-// User.cpp : header file
+ï»¿// User.cpp : header file
 //
 /////////////////////////////////////////////////////////////////////////////
 
@@ -117,8 +117,6 @@ Network Netzwerk;
 vector<Bauteil*> Bauteile;
 vector<Bauteil*> serial_Bauteile;
 
-void SternFind(vector<vector<string>> AdjazenzMatrix, string Pins);
-
 void Is_parallel() {
 	int outerSweep = Bauteile.size();
 	int innerSweep = Bauteile.size();
@@ -136,7 +134,7 @@ void Is_parallel() {
 
 						Bauteile.push_back(new Bauteil(Bauteile.at(i)->Name + "||" + Bauteile.at(ii)->Name,
 							Bauteile.at(i)->Art, Bauteile.at(i)->Pin1, Bauteile.at(i)->Pin2));				//New Element with outer Pins and new Name
-
+						cout << "Zusammenfassung: " << Bauteile.at(i)->Name << "||" << Bauteile.at(ii)->Name << endl;
 
 					if (i < ii) {
 						Bauteile.erase(Bauteile.begin() + i);													//Deleting obsolete Elements
@@ -205,7 +203,8 @@ void find_outer_Pins(string serial_Pin) {
 void create_new_serial_Bauteil() {
 
 			Bauteile.push_back(new Bauteil("(" + serial_Bauteile.at(0)->Name + " + " + serial_Bauteile.at(1)->Name + ")",
-				serial_Bauteile.at(1)->Art, serial_Bauteile.at(0)->Pin1, serial_Bauteile.at(1)->Pin1));				//New Element with outer Pins and new Name
+											serial_Bauteile.at(1)->Art, serial_Bauteile.at(0)->Pin1, serial_Bauteile.at(1)->Pin1));				//New Element with outer Pins and new Name
+			cout << "Zusammenfassung: " << serial_Bauteile.at(0)->Name << "+" << serial_Bauteile.at(1)->Name << endl;
 
 		for (int i = 0; i < Bauteile.size(); i++) {
 			if (!(serial_Bauteile.at(0)->Name.compare(Bauteile.at(i)->Name)) || (!(serial_Bauteile.at(1)->Name.compare(Bauteile.at(i)->Name)))) {
@@ -223,41 +222,71 @@ void create_new_serial_Bauteil() {
 void Is_serial() {
 
 	vector<string*> serial_Pins;
-	int c = 0;
-	int PinFlag = 0;
+int c = 0;
+int PinFlag = 0;
 
-	for (int i = 0; i < Bauteile.size(); i++) {															//Zuordnung äußere Pins, gemeinsamer Pin;
-		PinFlag = 0;
-		if (Is_serial_Pin(Bauteile.at(i)->Pin1)) {
+for (int i = 0; i < Bauteile.size(); i++) {															//Zuordnung Ã¤uÃŸere Pins, gemeinsamer Pin;
+	PinFlag = 0;
+	if (Is_serial_Pin(Bauteile.at(i)->Pin1)) {
 
-			for (int ii = 0; ii < serial_Pins.size(); ii++) {
-				if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin1))) PinFlag = 1;
-			}
-
-			if (PinFlag == 0) {
-				serial_Pins.push_back(new string(Bauteile.at(i)->Pin1));
-			}
+		for (int ii = 0; ii < serial_Pins.size(); ii++) {
+			if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin1))) PinFlag = 1;
 		}
-		else if (Is_serial_Pin(Bauteile.at(i)->Pin2)) {
 
-			for (int ii = 0; ii < serial_Pins.size(); ii++) {
-				if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin2))) PinFlag = 1;
-			}
-			if (PinFlag == 0) {
-				serial_Pins.push_back(new string(Bauteile.at(i)->Pin2));
-			}
+		if (PinFlag == 0) {
+			serial_Pins.push_back(new string(Bauteile.at(i)->Pin1));
 		}
 	}
-	for (int i = 0; i < serial_Pins.size(); i++) {
-		find_serial_Bauteil(*serial_Pins.at(i));
-		reverse_find_serial_Bauteil(*serial_Pins.at(i));
-		find_outer_Pins(*serial_Pins.at(i));
-		create_new_serial_Bauteil();
+	else if (Is_serial_Pin(Bauteile.at(i)->Pin2)) {
+
+		for (int ii = 0; ii < serial_Pins.size(); ii++) {
+			if (!(serial_Pins.at(ii)->compare(Bauteile.at(i)->Pin2))) PinFlag = 1;
+		}
+		if (PinFlag == 0) {
+			serial_Pins.push_back(new string(Bauteile.at(i)->Pin2));
+		}
 	}
+}
+for (int i = 0; i < serial_Pins.size(); i++) {
+	find_serial_Bauteil(*serial_Pins.at(i));
+	reverse_find_serial_Bauteil(*serial_Pins.at(i));
+	find_outer_Pins(*serial_Pins.at(i));
+	create_new_serial_Bauteil();
+}
 
 }
 
-void Adjazenz() {
+vector<Bauteil*> SternFind(vector<vector<Bauteil*>> AdjazenzMatrix, string Pins) {
+	vector<Bauteil*> SternBauteile;
+
+	int SternCounter = 0;
+	int rows = Pins.size();
+	int cols = Pins.size();
+
+	for (int outer = 0; outer < rows; outer++) {
+		for (int i = 0; i < rows; i++) {
+			if (AdjazenzMatrix[i][outer] != NULL) {
+				SternCounter++;
+				SternBauteile.push_back(AdjazenzMatrix[i][outer]);
+			}
+			if (AdjazenzMatrix[outer][i] != NULL) {
+				SternCounter++;
+				SternBauteile.push_back(AdjazenzMatrix[outer][i]);
+			}
+		}
+
+		if (SternCounter == 3) {
+			if ((Pins.substr(outer, 1) != Netzwerk.CMN) &&
+				(Pins.substr(outer, 1) != Netzwerk.OUTPUT) &&
+				(Pins.substr(outer, 1) != Netzwerk.INPUT)) return SternBauteile;
+		}
+		SternCounter = 0;
+		SternBauteile.clear();
+	}
+}
+
+void SternAdjazenz() {
+	vector<Bauteil*> SternBauteile;
 
 	string Pins = "";
 	for (int i = 0; i < Bauteile.size(); i++) {
@@ -272,35 +301,140 @@ void Adjazenz() {
 	int rows = Pins.size();
 	int cols = Pins.size();
 
-	vector<vector<string>> AdjazenzMatrix;
+	vector<vector<Bauteil*>> AdjazenzMatrix;
 	AdjazenzMatrix.resize(rows);
 	for (int i = 0; i < rows; i++) {
-		AdjazenzMatrix[i].resize(cols);;
+		AdjazenzMatrix[i].resize(cols);
 	}
 
 	for (int i = 0; i < Bauteile.size(); i++) {
 
-		AdjazenzMatrix[Pins.find(Bauteile.at(i)->Pin1)][Pins.find(Bauteile.at(i)->Pin2)] = Bauteile.at(i)->Name;
+		AdjazenzMatrix[Pins.find(Bauteile.at(i)->Pin1)][Pins.find(Bauteile.at(i)->Pin2)] = Bauteile.at(i);
 	}
-	SternFind(AdjazenzMatrix, Pins);
-}
-void SternFind(vector<vector<string>> AdjazenzMatrix, string Pins){
-	int SternCounter = 0;
-	int rows = Pins.size();
-	int cols = Pins.size();
 
-	for (int outer = 0; outer < rows; outer++) {
-		for (int i = 0; i < rows; i++) {
-			if (AdjazenzMatrix[i][outer] != "")SternCounter++;
-			if (AdjazenzMatrix[outer][i] != "")SternCounter++;
-		}
+	SternBauteile = SternFind(AdjazenzMatrix, Pins);
 
-		if (SternCounter == 3) {
-			break;
+	string SternPin;
+
+	if (SternBauteile.at(0)->Pin1 == SternBauteile.at(1)->Pin1)SternPin = SternBauteile.at(0)->Pin1;
+	else if (SternBauteile.at(0)->Pin1 == SternBauteile.at(1)->Pin2)SternPin = SternBauteile.at(0)->Pin1;
+	else SternPin = SternBauteile.at(0)->Pin2;
+
+	Pins.erase(Pins.find(SternPin), 1);
+
+	string* ValueBauteile = new string[Pins.size()];
+
+	for (int i = 0; i < SternBauteile.size(); i++) {
+		for (int ii = 0; ii < Pins.size(); ii++) {
+			if (SternBauteile.at(i)->Pin1 == Pins.substr(ii, 1)) ValueBauteile[ii] = SternBauteile.at(i)->Name;
+			else if (SternBauteile.at(i)->Pin2 == Pins.substr(ii, 1))ValueBauteile[ii] = SternBauteile.at(i)->Name;
 		}
-		SternCounter = 0;
+	}
+
+
+	Bauteile.push_back(new Bauteil("ZD" + Pins.substr(0, 1) + Pins.substr(1, 1), "Z",
+		Pins.substr(0, 1), Pins.substr(1, 1)));
+	cout << "Zusammenfassung: " << "ZD" + Pins.substr(0, 1) + Pins.substr(1, 1) <<
+		" = " << ValueBauteile[0] << " + " << ValueBauteile[1] << " + " <<
+		"(" << ValueBauteile[0] << "*" << ValueBauteile[1] << ")" << "/" << ValueBauteile[2] << endl;
+
+	Bauteile.push_back(new Bauteil("ZD" + Pins.substr(0, 1) + Pins.substr(2, 1), "Z",
+		Pins.substr(0, 1), Pins.substr(2, 1)));
+	cout << "Zusammenfassung: " << "ZD" + Pins.substr(0, 1) + Pins.substr(2, 1) <<
+		" = " << ValueBauteile[0] << " + " << ValueBauteile[2] << " + " <<
+		"(" << ValueBauteile[0] << "*" << ValueBauteile[2] << ")" << "/" << ValueBauteile[1] << endl;
+
+	Bauteile.push_back(new Bauteil("ZD" + Pins.substr(1, 1) + Pins.substr(2, 1), "Z",
+		Pins.substr(1, 1), Pins.substr(2, 1)));
+	cout << "Zusammenfassung: " << "ZD" + Pins.substr(1, 1) + Pins.substr(2, 1) <<
+		" = " << ValueBauteile[1] << " + " << ValueBauteile[2] << " + " <<
+		"(" << ValueBauteile[1] << "*" << ValueBauteile[2] << ")" << "/" << ValueBauteile[0] << endl;
+
+	int Size = Bauteile.size();
+	for (int i = 0; i < Size; i++) {
+		if (Bauteile.at(i)->Name == SternBauteile.at(0)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		else if (Bauteile.at(i)->Name == SternBauteile.at(1)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		else if (Bauteile.at(i)->Name == SternBauteile.at(2)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		Size = Bauteile.size();
 	}
 }
+bool DreieckAdjazenz() {
+	vector<Bauteil*> DreieckBauteile;
+
+	string Pins = "";
+	for (int i = 0; i < Bauteile.size(); i++) {
+
+		if (Pins.find(Bauteile.at(i)->Pin1) == Pins.npos) {
+			Pins = Pins + Bauteile.at(i)->Pin1;
+		}
+		if (Pins.find(Bauteile.at(i)->Pin2) == Pins.npos) {
+			Pins = Pins + Bauteile.at(i)->Pin2;
+		}
+	}
+
+	if (Pins.size() != 3)return false;
+
+	DreieckBauteile = Bauteile;
+
+	vector<vector<string>> ValueBauteile;
+	ValueBauteile.resize(Pins.size());
+
+	for (int i = 0; i < DreieckBauteile.size(); i++) {
+		for (int ii = 0; ii < Pins.size(); ii++) {
+			if (DreieckBauteile.at(i)->Pin1 == Pins.substr(ii, 1)) ValueBauteile[ii].push_back(DreieckBauteile.at(i)->Name);
+			else if (DreieckBauteile.at(i)->Pin2 == Pins.substr(ii, 1))ValueBauteile[ii].push_back(DreieckBauteile.at(i)->Name);
+		}
+	}
+
+			Bauteile.push_back(new Bauteil("Z*" + Pins.substr(0, 1), "Z",
+											Pins.substr(0, 1), "*"));
+			cout << "Zusammenfassung: " << "Z*" + Pins.substr(0, 1) <<
+											" = " << "(" << ValueBauteile[0][0] << "*" << ValueBauteile[0][1] << ")" << "/" <<
+											"(" << DreieckBauteile.at(0)->Name << " + " << DreieckBauteile.at(1)->Name << 
+											" + " << DreieckBauteile.at(2)->Name << ")" << endl;
+
+			Bauteile.push_back(new Bauteil("Z*" + Pins.substr(1, 1), "Z",
+											Pins.substr(1, 1), "*"));
+			cout << "Zusammenfassung: " << "Z*" + Pins.substr(1, 1) <<
+											" = " << "(" << ValueBauteile[1][0] << "*" << ValueBauteile[1][1] << ")" << "/" <<
+											"(" << DreieckBauteile.at(0)->Name << " + " << DreieckBauteile.at(1)->Name << 
+											" + " << DreieckBauteile.at(2)->Name << ")" << endl;
+
+			Bauteile.push_back(new Bauteil("Z*" + Pins.substr(2, 1), "Z",
+											Pins.substr(2, 1), "*"));
+			cout << "Zusammenfassung: " << "Z*" + Pins.substr(2, 1) <<
+											" = " << "(" << ValueBauteile[2][0] << "*" << ValueBauteile[2][1] << ")" << "/" <<
+											"(" << DreieckBauteile.at(0)->Name << " + " << DreieckBauteile.at(1)->Name << 
+											" + " << DreieckBauteile.at(2)->Name << ")" << endl;
+
+	int Size = Bauteile.size();
+	for (int i = 0; i < Size; i++) {
+		if (Bauteile.at(i)->Name == DreieckBauteile.at(0)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		else if (Bauteile.at(i)->Name == DreieckBauteile.at(1)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		else if (Bauteile.at(i)->Name == DreieckBauteile.at(2)->Name) {
+			Bauteile.erase(Bauteile.begin() + i);
+			i = -1;
+		}
+		Size = Bauteile.size();
+	}
+	return true;
+}
+
 
 
 void THE_ALGORITHM() {
@@ -318,10 +452,14 @@ void THE_ALGORITHM() {
 			Is_parallel();
 			parallelLimit = Bauteile.size();
 		}
+
+		if (Bauteile.size() == 3) {
+			if (DreieckAdjazenz())return;
+		}
+		else if (Bauteile.size() < 3)return;
+
+		SternAdjazenz();
 	}
-
-	Adjazenz();
-
 }
 
 
@@ -340,7 +478,7 @@ string NetworkInput() {
 	//getline(cin, InputBuffer);
 	//cout << "Registered String:\t" << InputBuffer << endl;
 
-	InputBuffer = "a:IN; b:Out; c: CMN; d,e,f,g: Internal;";			// For Testing only
+	InputBuffer = "a:IN; b:Out; c: CMN; d,e: Internal;";			// For Testing only
 
 	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
 		if (isspace(InputBuffer[spacepos])) {
@@ -377,7 +515,7 @@ string BauteilInput() {
 	//getline(cin, InputBuffer);
 	//cout << "Registered String:\t" << InputBuffer << endl;
 
-	InputBuffer = "R1:R(a, d); R2:R(e, f); R3:R(g, b); L1:L(f, c); C1:C(d,e); C2:C(e, g); C3:C(g, b); ";			// For Testing only 
+	InputBuffer = "R1:R(a, e); R2:R(e, d); R3:R(d, b); R4:R(e, b); L1:L(a, d); L2:L(e, c); C1:C(a,c); C2:C(e,b); ";			// For Testing only 
 	for (spacepos; spacepos < InputBuffer.size(); spacepos++) {
 		if (isspace(InputBuffer[spacepos])) {
 			spacepos++;
